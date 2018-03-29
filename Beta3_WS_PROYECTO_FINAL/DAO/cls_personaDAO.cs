e44d9644d8af337d7b2e7c7d3e4dfcae0a069993
gaspar <gaspar@DELTA1>
2018-03-29 12:29:27 -0600

    Eventos de Organizador en personaDAO completo

--- Beta3_WS_PROYECTO_FINAL/DAO/cls_personaDAO.cs
+++ Beta3_WS_PROYECTO_FINAL/DAO/cls_personaDAO.cs
@@ -27,12 +27,12 @@ namespace Beta3_WS_PROYECTO_FINAL.DAO
 
         public int AgregarDt(cls_personaBO obj_persona)
         {
-            string ComandoSQL = string.Format("INSERT INTO  persona (matricula,Nombre,Apelido_Paterno,Apelido_Materno,Correo_Electronico,Telefono,Genero,tipo_persona,id_usuario,Idequipo,)VALUES ({0},'{1}','{2}',{3},'{4}','{5}',{6},{7},{8},{9});", obj_per.Matricula_persona, obj_persona.Nombre_persona, obj_persona.ApePat_persona, obj_persona.ApeMar_persona, obj_persona.CorreoElect_persona, obj_persona.Telefono_persona, obj_persona.Genero_persona, obj_persona.Tipo_persona, obj_persona.Id_usuario, obj_persona.Id_equipo, obj_persona.Id_convocatoria);
+            string ComandoSQL = string.Format("INSERT INTO  persona (matricula,Nombre,Apelido_Paterno,Apelido_Materno,Correo_Electronico,Telefono,Genero,tipo_persona,id_usuario,Idequipo,)VALUES ({0},'{1}','{2}',{3},'{4}','{5}',{6},{7},{8},{9});", obj_per.Matricula_persona, obj_persona.Nombre_persona, obj_persona.ApePat_persona, obj_persona.ApeMar_persona, obj_persona.CorreoElect_persona, obj_persona.Telefono_persona, obj_persona.Genero_persona, obj_persona.Tipo_persona, obj_persona.Id_usuario, obj_persona.Id_equipo);
             return Miconexion.EjecutarComando(ComandoSQL);
         }
         public int ModificarDt(cls_personaBO obj_persona)
         {
-            string ComandoSQL = string.Format("UPDATE persona SET Nombre='{1}',Apelido_Paterno='{2}',Apelido_Materno='{3}',Correo_Electronico='{4}',Telefono='{5}',Genero={6},tipo_persona={7},id_usuario={8},Idequipo={9}", obj_persona.Matricula_persona, obj_persona.Nombre_persona, obj_persona.ApePat_persona, obj_persona.ApeMar_persona, obj_persona.CorreoElect_persona, obj_persona.Telefono_persona, obj_persona.Genero_persona, obj_persona.Tipo_persona, obj_persona.Id_usuario, obj_persona.Id_equipo, obj_persona.Id_convocatoria);
+            string ComandoSQL = string.Format("UPDATE persona SET Nombre='{1}',Apelido_Paterno='{2}',Apelido_Materno='{3}',Correo_Electronico='{4}',Telefono='{5}',Genero={6},tipo_persona={7},id_usuario={8},Idequipo={9}", obj_persona.Matricula_persona, obj_persona.Nombre_persona, obj_persona.ApePat_persona, obj_persona.ApeMar_persona, obj_persona.CorreoElect_persona, obj_persona.Telefono_persona, obj_persona.Genero_persona, obj_persona.Tipo_persona, obj_persona.Id_usuario, obj_persona.Id_equipo);
             return Miconexion.EjecutarComando(ComandoSQL);
         }
         public int EliminarDt(cls_personaBO obj_persona)
@@ -40,7 +40,21 @@ namespace Beta3_WS_PROYECTO_FINAL.DAO
             string ComandoSQL = string.Format("DELETE FROM persona WHERE matricula= {0}", obj_per.Matricula_persona);
             return Miconexion.EjecutarComando(ComandoSQL);
         }
-
+        public int AgregarOrganizador(cls_personaBO obj_persona)
+        {
+            string ComandoSQL = string.Format("INSERT INTO  persona (matricula,Nombre,Apelido_Paterno,Apelido_Materno,Correo_Electronico,Telefono,Genero,tipo_persona,id_usuario,Idconvocatoria,)VALUES ({0},'{1}','{2}',{3},'{4}','{5}',{6},{7},{8},{10});", obj_per.Matricula_persona, obj_persona.Nombre_persona, obj_persona.ApePat_persona, obj_persona.ApeMar_persona, obj_persona.CorreoElect_persona, obj_persona.Telefono_persona, obj_persona.Genero_persona, obj_persona.Tipo_persona, obj_persona.Id_usuario,obj_persona.Id_convocatoria);
+            return Miconexion.EjecutarComando(ComandoSQL);
+        }
+        public int ModificarOrganizador(cls_personaBO obj_persona)
+        {
+            string ComandoSQL = string.Format("UPDATE persona SET Nombre='{1}',Apelido_Paterno='{2}',Apelido_Materno='{3}',Correo_Electronico='{4}',Telefono='{5}',Genero={6},tipo_persona={7},id_usuario={8},Idconvocatoria={10}", obj_persona.Matricula_persona, obj_persona.Nombre_persona, obj_persona.ApePat_persona, obj_persona.ApeMar_persona, obj_persona.CorreoElect_persona, obj_persona.Telefono_persona, obj_persona.Genero_persona, obj_persona.Tipo_persona, obj_persona.Id_usuario, obj_persona.Id_convocatoria);
+            return Miconexion.EjecutarComando(ComandoSQL);
+        }
+        public int EliminarOrganizador(cls_personaBO obj_persona)
+        {
+            string ComandoSQL = string.Format("DELETE FROM persona WHERE matricula= {0}", obj_per.Matricula_persona);
+            return Miconexion.EjecutarComando(ComandoSQL);
+        }
 
     }
 }
