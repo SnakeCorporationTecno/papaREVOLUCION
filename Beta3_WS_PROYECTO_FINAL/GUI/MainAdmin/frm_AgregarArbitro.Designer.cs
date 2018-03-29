ce4613d1e5488c25fc5a3f883fd46b9331c07e93
Bryan Edilberto Pool Mercado <SnakeTecno@hotmail.com>
2018-03-29 13:46:01 -0600

    Agregue partidos DAO, y el form Agregar Arbitro

---
+++ Beta3_WS_PROYECTO_FINAL/GUI/MainAdmin/frm_AgregarArbitro.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Beta3_WS_PROYECTO_FINAL.GUI.MainAdmin
+{
+    partial class frm_AgregarArbitro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(846, 44);
+            this.panel1.TabIndex = 0;
+            // 
+            // frm_AgregarArbitro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 451);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_AgregarArbitro";
+            this.Text = "frm_AgregarArbitro";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
